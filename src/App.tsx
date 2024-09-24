9f90ec21fff704dea76dcba0f6b58365dc8ad0ab
varaan785@gmail.com <156232089+Varaan785@users.noreply.github.com>
2024-09-24 22:54:04 +0200

    add hw06

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,27 @@
 
+
+
+
+
+//HOMEWORKS
+import Homework06 from 'homeworks/Homework_06';
+//LESSONS
+import Lesson06 from 'lessons/Lesson06/Lesson06';
+//CONSULTATIONS
+
+
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App"> React TypscriptApp </div>
+    <div className="App"> React TypscriptApp
+    
+    {/* <Lesson06/> */}
+
+    <Homework06 />
+    
+     </div>
   );
 }
 
